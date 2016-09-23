72ef61c0fe4db610eb32b07bb0b5352cc392f976
Pavan <kpavan.kumar36@gmail.com>
2016-09-22 23:04:44 -0500

    updating reading testing and training images and labels

--- MulticategoryPTA.py
+++ MulticategoryPTA.py
@@ -5,45 +5,48 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # Reading input from the images
-np.set_printoptions(threshold=np.inf)
-flbl = open("train-labels.idx1-ubyte", 'rb')
-magic_nr, size_label = struct.unpack(">II", flbl.read(8))
-lbl = pyarray("b", flbl.read())
-flbl.close()
+def getLabelsArray(fname, size_img, output_dim):
+    flbl = open(fname, 'rb')
+    magic_nr, size_label = struct.unpack(">II", flbl.read(8))
+    lbl = pyarray("b", flbl.read())
+    flbl.close()
 
-fimg = open("train-images.idx3-ubyte", 'rb')
-magic_nr, size_img, rows, cols = struct.unpack(">IIII", fimg.read(16))
-img = pyarray("B", fimg.read())
-img = np.asarray(img).reshape(60000, 784)
-fimg.close()
+    desired_label = np.zeros((size_img, output_dim), dtype=np.int)
+    for i in range(size_label):
+        desired_label[i][lbl[i]] = 1
 
-desired_label = np.zeros((size_img,10), dtype=np.int)
-for i in range(size_label):
-	desired_label[i][lbl[i]] = 1
+    return desired_label
 
+def getImagesArray(fname):
+    fimg = open(fname, 'rb')
+    magic_nr, size_img, rows, cols = struct.unpack(">IIII", fimg.read(16))
+    img = pyarray("B", fimg.read())
+    img = np.asarray(img).reshape(size_img, rows*cols)
+    fimg.close()
+    return img, size_img
 # end of reading input
 
-epoch = 0
-n = 1
-e = 0.1
-errors = []
-epochs = []
+
 w = np.random.rand(10,784)
 # x = np.random.rand(6000, 784,1)
 # d = np.random.rand(6000,10,1)
 # v = np.random.rand(10,1)
-def MPTA():
+def MPTA(no_of_TrainingImages):
     global epoch, w, img
+    epoch = 0
+    errors = []
+    epochs = []
     toContinue = True
     # getting the no of images
-    #i = 5000
-    i = img.shape[0]
+    i = no_of_TrainingImages
+    #i = img.shape[0]
     while(toContinue):
-        print("epoch :: ", epoch)
+        #print("epoch :: ", epoch)
         errors.append(0)
         for index in range(0, i):
-            if index%10000 == 0:
-                print(index, " elements done")
+            #if index%10000 == 0:
+                #print(index, " elements done")
+            #print("printing image ", img[0])
             x = img[index].reshape(784,1)
             d = desired_label[index].reshape(10,1)
             #errors[epoch]=0
@@ -72,9 +75,9 @@ def MPTA():
         # cehcking if the minimum error ratio is reached or not
         if (errors[epoch-1]/i < e):
             toContinue = False
-            print("error rate :: ", errors[epoch - 1] / i)
+            print("epoch ", epoch, " error rate :: ", errors[epoch - 1] / i)
         else:
-            print("errors not meeting standards. error rate :: ", errors[epoch-1]/i)
+            print("epoch ", epoch, " errors not meeting standards. error rate :: ", errors[epoch-1]/i)
 
     print(" optimal weights found after", epoch, " epochs")
     plt.plot(epochs, errors, 'b')
@@ -83,13 +86,13 @@ def MPTA():
 
 # Testing
 def testImages():
-    i = img.shape[0]
+    i = testImg.shape[0]
     misClassErrorsInTesting = 0
     for index in range(0, i):
         if index%10000 == 0:
             print(index, " elements done")
-        x = img[index].reshape(784,1)
-        d = desired_label[index].reshape(10,1)
+        x = testImg[index].reshape(784,1)
+        d = test_desired_label[index].reshape(10,1)
         #errors[epoch]=0
         v = w.dot(x)
 
@@ -97,7 +100,7 @@ def testImages():
         v = getVectorWithOneAtIndex(maxIndex)
         if (not (d==v).all()):
             misClassErrorsInTesting = misClassErrorsInTesting + 1
-    print (misClassErrorsInTesting)
+    print ("no of misclaffications", misClassErrorsInTesting)
 
 
 
@@ -141,9 +144,39 @@ v[5][0] = -1
 #print (getVectorWithOneAtIndex(3))
 
 # Training the network
-MPTA()
+img, size_img = getImagesArray("train-images.idx3-ubyte")
+desired_label = getLabelsArray("train-labels.idx1-ubyte", size_img, 10)
 
-#
+testImg, test_size_img  = getImagesArray("t10k-images.idx3-ubyte")
+test_desired_label = getLabelsArray("t10k-labels.idx1-ubyte", test_size_img, 10)
+
+#STEP - f
+no_of_TrainingImages = 50
+n = 1
+e=0.05
+MPTA(no_of_TrainingImages)
+testImages()
+
+#STEP - g
+no_of_TrainingImages = 1000
+n = 1
+e=0.05
+MPTA(no_of_TrainingImages)
+testImages()
+
+#STEP - h
+no_of_TrainingImages = 60000
+n = 1
+e = 0
+MPTA(no_of_TrainingImages)
+testImages()
+
+#STEP - i
+no_of_TrainingImages = 60000
+n = 1
+e = 0.1
+MPTA(no_of_TrainingImages)
+testImages()
 
 
 
