60b8a27d9b295fd5acd4089ebeebdc5712749737
Pavan <kpavan.kumar36@gmail.com>
2017-03-06 20:01:59 -0800

    implemented hopfield networks

---
+++ hopfieldNetwork.py
@@ -0,0 +1,23 @@
+import itertools
+import numpy as np
+lst = list(itertools.product([-1, 1], repeat=3))
+inputs = np.array(lst)
+#w = np.array([[0, 2, -1], [2, 0, -1], [-1, -1, 1]])
+w = np.array([[1, -1, -1], [-1, 0, 2], [-1, 2, 0] ])
+b = np.array([0.5, 0.5, 0.5])
+b = b.reshape(3,1)
+biasTranspose = b.reshape(1,3)
+for row in inputs:
+    curip = row.reshape(3,1)
+    #print (curip.reshape(1,3))
+    res = (np.dot(w,curip) + b).reshape(1,3)
+    curipTranspose = curip.reshape(1,3)
+
+    e1 = -np.dot(curipTranspose, np.dot(w,curip)) - 2*np.dot(biasTranspose, curip)
+    print(e1)
+    #print(res)
+    res =  [1 if(x>=0) else -1 for x in res[0]]
+    #e2 = np.dot(curipTranspose, np.dot(w, curip)) - 2 * np.dot(biasTranspose, curip)
+    print (curip.reshape(1,3), " -> ", res)
+    print("***********")
+    #testing git commit --amend
